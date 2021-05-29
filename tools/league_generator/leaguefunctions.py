e68a40b7b13cd16f408ad099dfb3e93a9c97f6dd
Liam Treutel <wjtreutel@crimson.ua.edu>
2021-05-29 13:39:25 -0700

    Completed single-season standings, working on support for multiple years

--- tools/league_generator/leaguefunctions.py
+++ tools/league_generator/leaguefunctions.py
@@ -1,37 +1,50 @@
-from random import randint
+# TeamGenerator.py
+#
+# Created: 5/29/21
+#
+# Library file for high-level team functions
 
+
+from random import randint,shuffle
+
+
+# I legitimately can't remember if class creators are private,
+# so I hastily made this wrapper function
+# and I need to fix this later
 def new_league(names):
     return League(names)
+
 
 
 class League(object):
     def __init__(self,name_list):
         self.name = 'Example Baseball League'
-        self.teams = self.generate_teams(name_list)
+        self.teams = self.generate_teams(generate_names(6))
+        self.former_teams = list()
+        self.total_number_of_teams = 0
+
+        # Allows schedule to be referred to abstractly
+        # Might be useful in the future
+        self.schedule_system = self.simple_round_robin
+        self.schedule_rounds = 11
+
+        self.years = dict()
+
+
+
+
+
 
 
     def generate_teams(self,names):
         team_list = list()
+        x = 0
 
         for name in names:
-            team_list.append(Team(name))
+            team_list.append(Team(self,name))
 
         return team_list
 
-
-    def play_round_robin(self):
-        n = len(self.teams) - 1
-
-        home = self.teams
-        away = self.teams
-
-
-        for i in range(n):
-            away = away[n:] + away[:n]
-            round = zip(home,away)
-
-            for match in round:
-                self.play_game(match[0],match[1])
 
     def play_game(self,home_team,away_team):
             home_team.results['HomeGames'] += 1
@@ -57,17 +70,55 @@ class League(object):
             loser.results['L']  += 1
 
 
+    def simple_round_robin(self):
+        n = len(self.teams) - 1
 
+        home = self.teams
+        away = self.teams
+
+        for i in range(n):
+            away = away[n:] + away[:n]
+            round = zip(home,away)
+
+            for match in round:
+                self.play_game(match[0],match[1])
+
+
+    def get_rankings(self):
+        return sorted(self.teams, key= lambda x: x.results['W'],reverse = True)
+
+
+    # IDEA: configure with divisions, each division is 1/3 of screen
+    def display_standings(self):
+        # Sort teams by win total, display display_standings
+
+        print "    {:20} Standings {}".format(self.name,randint(1885,2020))
+        print "    ======================================="
+
+        print "              Name           |  W  |  L  |  %  "
+        for team in self.teams:
+            win_percentage = float(team.results['W']) / (team.results['W'] + team.results['L'])
+            print "-----------------------------------------------"
+            print "{:28} | {:>3} | {:>3} | {:.3f}".format(team.name,team.results['W'],team.results['L'],win_percentage)
+
+
+#
+# TEAM OBJECT
+#
 
 class Team(object):
-    def __init__(self,name):
+    def __init__(self,league,id,name):
+        self.league = league
         self.name = name
+        self.id = id
         self.results = dict()
         self.team_score = randint(20,80)
         for attribute in ['W','L','HomeGames','AwayGames']:
             self.results[attribute] = 0
 
 
+    def disband_team(self):
+        print "[disband_team] not implemented"
 
 
 # Function taken from Makis on StackOverflow
@@ -93,3 +144,25 @@ def create_schedule(list):
         list.insert(1, list.pop())
 
     return s
+
+
+def generate_names(x):
+    cities = [
+    'Chicago','Indianapolis','Bloomington','St. Louis',
+    'Boston','Seattle','Portland','Nashville']
+
+    names = ['Robins','Larks','Warblers','Cardinals','Wrens',
+             'Athletics','Baseball Club','All-Stars','Millionaires',
+             'Seadogs','Men-of-War','Corsairs','Pirates',
+             'Hounds','Bulldogs','Kingfishers','Harriers']
+
+    shuffle(names)
+
+    results = []
+
+    for i in range(x):
+        curr = "{} {}".format(cities[randint(0,len(cities)-1)],
+                              names.pop())
+        results.append(curr)
+
+    return results
